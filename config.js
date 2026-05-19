cb7c180a3beffc7fa37846cca20f1d9d4d9fce80
Juyel Hossain <luxlang.org@gmail.com>
2026-05-19 11:54:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,25 @@
-const CONFIG={
-APP_NAME:"Cooch Behar Healthcare",
-CSV_URL:"https://docs.google.com/spreadsheets/d/1R0iYUyB38m0dAhNhQfx5w2TVMAHIQ3Y1fWxvDlA-TWM/export?format=csv&gid=0",
-SHEET_TAB:"TAB01",
-GAS_URL:"Place_Holder",
-TG_BOT:"8935806264:AAHVB9rcx0hsu5jVBuEUVpQqw5MYba7FxR0",
-TG_CHAT:"7929275539",
-VISITOR_REFRESH:30000,
-SEARCH_DELAY:300,
-VERSION:"1.0.0"
-}
+const CONFIG = {
+  SHEET_CSV: "https://docs.google.com/spreadsheets/d/1R0iYUyB38m0dAhNhQfx5w2TVMAHIQ3Y1fWxvDlA-TWM/export?format=csv&gid=0",
+  GAS_URL: "Place_Holder",
+  TG: {
+    BOT: "8935806264:AAHVB9rcx0hsu5jVBuEUVpQqw5MYba7FxR0",
+    CHAT: "7929275539"
+  },
+  APP: {
+    NAME: "Cooch Behar Healthcare",
+    SHORT: "CBH Directory",
+    TAGLINE: "Community-powered healthcare directory",
+    CACHE: "cbhd-v1",
+    VISITOR_MS: 30000,
+    DEBOUNCE_MS: 300,
+    NOTICE_KEY: "cbhd_notice_dismissed",
+    SESSION_KEY: "cbhd_sid"
+  },
+  C: {
+    ID: 0, NAME: 1, SPEC: 2, DEG: 3,
+    CHAMBER: 4, ADDR: 5, AREA: 6, CITY: 7,
+    PHONE: 8, WA: 9, S1: 10, S2: 11,
+    S3: 12, NOTE: 13, FEES: 14,
+    VERIF: 15, BY: 16, AT: 17
+  }
+};
